33fa42e1ca7b404393174d097ca457257950a12d
Thomas Leupp <tomleupp@gmail.com>
2020-06-15 19:33:48 -0700

    refactor about screen

--- views/about.js
+++ views/about.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {
   StyleSheet,
@@ -12,28 +13,32 @@ import Toolbar from '../components/Toolbar';
 
 const About = () => {
   return (
-    <View style={{flex: 1}}>
+    <View style={styles.wrap}>
       <Toolbar iconAction={'back'} />
-      <LinearGradient colors={gradientColors} style={{flex:1}}>
+      <LinearGradient colors={gradientColors} style={styles.wrap}>
         <View style={styles.main}>
-          <View style={{flex: 1, margin: 10, height: 75, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+          <View style={styles.logo}>
             <Image source={require('../assets/logo_outline.png')} />
           </View>
           <View style={styles.container}>
-              <View style={{flex: 2, flexDirection: 'row',justifyContent: 'center', alignItems: 'center'}}>
-                <Text style={{fontSize: 22, textAlign: 'center'}}>
-                  Version 1.03
-                </Text>
-              </View>
-              <View style={{flex: 1, flexDirection: 'column',justifyContent: 'center', alignItems: 'center'}}>
-                <Text style={{fontSize: 16, textAlign: 'center'}}>
-                  <Image source={require('../assets/ic_copyright_black_24dp.png')} style={{height:20,width:20}} /> 2016 Thomas Leupp
-                </Text>
-                <Text></Text>
-                <Text style={{fontSize: 14, textAlign: 'center'}}>
-                  Beer data courtesy of BreweryDB
-                </Text>
-              </View>
+            <View style={styles.version}>
+              <Text style={{ fontSize: 22, textAlign: 'center' }}>
+                Version 1.03
+              </Text>
+            </View>
+            <View style={styles.copyright}>
+              <Text style={{ fontSize: 16, textAlign: 'center' }}>
+                <Image
+                  source={require('../assets/ic_copyright_black_24dp.png')}
+                  style={styles.copylogo}
+                />
+                2016 Thomas Leupp
+              </Text>
+              <Text></Text>
+              <Text style={{ fontSize: 14, textAlign: 'center' }}>
+                Beer data courtesy of BreweryDB
+              </Text>
+            </View>
           </View>
         </View>
       </LinearGradient>
@@ -44,34 +49,40 @@ const About = () => {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    flexDirection: 'column', 
+    flexDirection: 'column',
     justifyContent: 'space-around',
     alignItems: 'center',
   },
   container: {
     flex: 4,
-    justifyContent: 'center'
-  },
-  choose: {
-    fontSize: 27,
-    textAlign: 'center',
-    margin: 30,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  centering: {
-    flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-    padding: 2,
+  },
+  logo: {
+    flex: 1,
+    margin: 10,
+    height: 75,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  version: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  copyright: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  copylogo: {
+    height: 20,
+    width: 20,
+  },
+  wrap: {
+    flex: 1,
   },
 });
 
--- views/webview.js
+++ views/webview.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import {
-  WebView, 
+  WebView,
   StyleSheet,
   Text,
   View,
